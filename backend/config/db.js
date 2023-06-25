a80c3ac48ef48d004ce7888a26a57b67f1083b0d
jennerdulce <jennerdulce@gmail.com>
2023-06-25 13:56:26 -0700

    Not using colors package

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI)
 
-        console.log(`MongoDB Connected: ${ conn.connection.host}`.cyan.underline)
+        console.log(`MongoDB Connected: ${ conn.connection.host}`)
     } catch (error) {
         console.log(error)
         process.exit(1)
